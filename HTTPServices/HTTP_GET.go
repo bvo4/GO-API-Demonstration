b0eb5ba82427e34da646574e89f19aee20c60076
Brandon Vo <bvo@morrisdickson.com>
2025-04-04 17:09:02 -0500

    Ability to send HTTP GET Requests now functional

--- HTTPServices/HTTP_GET.go
+++ HTTPServices/HTTP_GET.go
@@ -2,7 +2,9 @@ package HTTPServices
 
 import (
 	HTTPServices "API_DEMONSTRATION/HTTPServices/Models"
-	"fmt"
+	"crypto/tls"
+	"io"
+	"log"
 	"net/http"
 )
 
@@ -14,14 +16,45 @@ func Write_HTTP_GET(OrderId string, API_Credentials *HTTPServices.HEADER_VALUES)
 	Send_HTTP_GET(URI, HEADER_PARAMS)
 }
 
+/*
+	Credit: https://blog.logrocket.com/making-http-requests-in-go/
+
+// https://stackoverflow.com/questions/54401790/how-can-i-use-http-1-x-in-client-and-server
+// https://stackoverflow.com/questions/57420833/tls-no-renegotiation-error-on-http-request
+*/
 func Send_HTTP_GET(URI string, HEADER_PARAMS map[string]string) {
 	//Get the Request URI using the OrderID
-
-	r, err := http.NewRequest("GET", URI, nil)
+	r, err := http.NewRequest(http.MethodGet, URI, nil)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(r)
+	/* HTTP CLIENT SETTINGS */
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+				Renegotiation:      tls.RenegotiateOnceAsClient,
+			},
+		},
+	}
 
+	AddHTTPHeaders(*r, HEADER_PARAMS)
+
+	response, err := client.Do(r)
+
+	if err != nil {
+		panic(err)
+	}
+
+	body, err := io.ReadAll(response.Body)
+	sb := string(body)
+	log.Printf(sb)
+}
+
+/* Credit: https://stackoverflow.com/questions/8018719/iterating-through-a-golang-map */
+func AddHTTPHeaders(r http.Request, HEADER_PARAMS map[string]string) {
+	for key, value := range HEADER_PARAMS {
+		r.Header.Add(key, value)
+	}
 }
