b992092512e63a4ff72466b4f74c4188c9e9ac4d
bvo4 <brandonvo1980@yahoo.com>
2025-04-03 22:04:00 -0500

    Ditto

--- HTTPServices/URI_Directory.go
+++ HTTPServices/URI_Directory.go
@@ -1,14 +1,11 @@
 package HTTPServices
 
-import "fmt"
-
 func BuildOrderIdURI(OrderId string) string {
 
 	if len(OrderId) < 1 {
 		return ""
 	}
 
-	RequestURI := fmt.Sprintf("https://onescan.lspedia.com/ext/api/orders/%s/itemstree", OrderId)
 	return RequestURI
 
 }
